a758a5cc7bf2eb8f86c5c149af22aa110722677d
venkatram64 <venkat.veerareddy@hotmail.com>
2024-02-11 17:42:08 +0530

    base security

--- my-bank/src/main/java/com/venkat/mybank/config/SecurityConfig.java
+++ my-bank/src/main/java/com/venkat/mybank/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +29,29 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults()) //built in login page is shown, form login is the login page for authentication
                 .httpBasic(Customizer.withDefaults()) //for postman
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User
+                .withUsername("venkat@gmail.com")
+                .password(passwordEncoder().encode("1234"))
+                .authorities("user")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("srijan@gmail.com")
+                .password(passwordEncoder().encode("1234"))
+                .authorities("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
