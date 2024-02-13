efd4a5eef1654cc9a23e79635edb440ce2bc9409
venkatram64 <venkat.veerareddy@hotmail.com>
2024-02-13 17:31:41 +0530

    dao security

--- my-bank/src/main/java/com/venkat/mybank/config/SecurityConfig.java
+++ my-bank/src/main/java/com/venkat/mybank/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.venkat.mybank.config;
 
-import com.venkat.mybank.service.CustomUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,18 +29,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        return new CustomUserDetailService();
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
 
 }
--- my-bank/src/main/java/com/venkat/mybank/service/UserService.java
+++ my-bank/src/main/java/com/venkat/mybank/service/UserService.java
@@ -10,7 +10,11 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User save(User user){
         return userRepository.save(user);
