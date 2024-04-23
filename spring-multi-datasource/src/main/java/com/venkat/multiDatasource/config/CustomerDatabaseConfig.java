23fdc99a4b61a78576724f7349cefe182c62887b
venkatram64 <venkat.veerareddy@hotmail.com>
2024-04-23 23:24:58 +0530

    authentication provider

--- spring-multi-datasource/src/main/java/com/venkat/multiDatasource/config/CustomerDatabaseConfig.java
+++ spring-multi-datasource/src/main/java/com/venkat/multiDatasource/config/CustomerDatabaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -62,5 +63,11 @@ public class CustomerDatabaseConfig {
     public PlatformTransactionManager customerTransactionManager(@Qualifier("customerEntityManagerFactory")
                                                                      LocalContainerEntityManagerFactoryBean entityManagerFactory){
         return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+
+    @Primary
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate jdbcTemplate(@Qualifier("customerDataSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
     }
 }
